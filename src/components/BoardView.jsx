1ed550a6de631280a3206bc7f7cb717427abede2
Tamas Tarjan <tarjan.nt+main@gmail.com>
2024-07-07 20:34:15 +0200

    Add root dnd

--- src/components/BoardView.jsx
+++ src/components/BoardView.jsx
@@ -5,9 +5,36 @@ import {
   updateBoard,
 } from "../context/BoardsContext";
 import { GraphNodeView } from "./GraphNodeView";
+import { useDrop } from "react-dnd";
+import { OrphansContext, updateOrphans } from "../context/OrphansContext";
 
 export function BoardView() {
   const [selectedBoard, setSelectedBoard] = useContext(BoardSelectionContext);
+  const [orphans, setOrphans] = useContext(OrphansContext);
+  const [collectedDropProps, drop] = useDrop(
+    () => ({
+      accept: ["OrphanNode"],
+      drop: (droppedNode, monitor) => {
+        if (monitor.getItemType() === "OrphanNode") {
+          const sourceIndex = orphans.findIndex((o) => o.id === droppedNode.id);
+          orphans.splice(sourceIndex, 1);
+          setOrphans(updateOrphans(orphans));
+
+          const itemNode = new ItemNode(
+            droppedNode.name,
+            selectedBoard.id,
+            selectedBoard.id
+          );
+
+          itemNode.children = droppedNode.children || [];
+          selectedBoard.children.push(itemNode);
+
+          setSelectedBoard(updateBoard(selectedBoard));
+        }
+      },
+    }),
+    [selectedBoard, orphans]
+  );
 
   const addNoteClicked = () => {
     const nodeName = prompt("Enter your note:");
@@ -25,7 +52,13 @@ export function BoardView() {
   return (
     <>
       {selectedBoard ? (
-        <div className="board-view">
+        <div
+          className="board-view"
+          ref={(el) => {
+            drop(el);
+          }}
+          {...collectedDropProps}
+        >
           <div className="board-view-title">
             <h1>{selectedBoard.name}</h1>
           </div>
@@ -36,12 +69,6 @@ export function BoardView() {
               })}
             </div>
             <br />
-            <button
-              className="add-note-button loose-action"
-              onClick={addNoteClicked}
-            >
-              New note
-            </button>
           </div>
         </div>
       ) : (
